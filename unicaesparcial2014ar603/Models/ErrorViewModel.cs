9630973141b283c7b0d2f438d3e111e9772a5a33
david.amaya <2014AR603@catolica.edu.sv>
2020-05-01 01:18:05 -0500

    Primer commit

---
+++ unicaesparcial2014ar603/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace unicaesparcial2014ar603.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
